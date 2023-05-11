df50b6dfdf800dec28f8768d6d4f0bec91cea6d2
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-05-11 01:44:17 +0100

    Removed PathType since we are not distinguishing them for output for now

--- src/gpath/_rules/__init__.py
+++ src/gpath/_rules/__init__.py
@@ -1,5 +1,2 @@
 from ._common import _COMMON_DRIVE_POSTFIX, _COMMON_CURRENT_INDICATOR, _COMMON_PARENT_INDICATOR
-from ._rules import (
-    _from_type,
-    _generic_rules,
-)
+from ._rules import _generic_rules
--- src/gpath/_rules/_rules.py
+++ src/gpath/_rules/_rules.py
@@ -1,6 +1,4 @@
-from typing import Type
 from ._common import _COMMON_DRIVE_POSTFIX, _COMMON_CURRENT_INDICATOR, _COMMON_PARENT_INDICATOR
-from ..pathtype import PathType
 
 from .._compat import Final
 
@@ -15,7 +13,3 @@ class _generic_rules(UnvalidatedRules):
 	roots: Final = ["/", "\\"]
 	separators: Final = ["/", "\\"]
 	drive_postfixes: Final = [":"]
-
-
-def _from_type(type: PathType) -> Type[UnvalidatedRules]:
-	return _generic_rules
